0df634b1d922dad616b56a27d1902c380b04b716
yzqzss <yzqzss@yandex.com>
2023-05-23 21:27:41 +0800

    * languages: better fallback * feed: generate ATOM as default * generate an ugly index.html

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # F-Droid_Newapps_RSS
 
-RSS: <https://newapps.f-droid.othing.xyz/new_apps.en-US.xml>
+> <https://newapps.f-droid.othing.xyz>
+
+Default feed format is `Atom`, you can add `.rss` before `.xml` to get RSS 2.0 format.  
 
 You can replace `en-US` with another language code. (see `languages_config.py`)
 
+## License
 
-# License
-WTFPLv2
+WTFPL v2
--- new_apps.py
+++ new_apps.py
@@ -75,43 +75,118 @@ with open('new_apps.json', 'w') as f:
     json.dump(sorted_new_packages, f, ensure_ascii=False, indent=4)
     print('Done.')
 
+
+os.makedirs('feed/', exist_ok=True)
+
 from languages_config import langs
 for lang in langs:
-    print('Generating RSS feed...', lang)
+    primary_lang = lang.split('-')[0]
+    area_lang = lang.split('-')[1] if len(lang.split('-')) > 1 else None
+    print('Generating ATOM feed...', lang)
     feed = FeedGenerator()
-    feed.title('New apps on F-Droid')
-    feed.description('New apps on F-Droid')
-    feed.link(href='https://f-droid.org/packages/')
+    feed.title(f'New apps on F-Droid ({lang})')
+    feed.description(f'New apps on F-Droid ({lang})')
+    feed.id(f'https://newapps.f-droid.othing.xyz/new_apps.{lang}.xml')
+    feed.link( href=f'https://newapps.f-droid.othing.xyz/new_apps.{lang}.xml', rel='self' )
+    feed.link(href='https://f-droid.org/packages/', rel='alternate')
+    feed.author({'name': 'yzqzss', 'email': 'yzqzss@yandex.com'})
     feed.language(lang)
     for package_name in sorted_new_packages:
         print(package_name)
         package = sorted_new_packages[package_name]
         entry = feed.add_entry(order='append')
-        entry.guid(package_name, permalink=True)
+        entry.id(package_name)
         entry.pubDate(datetime.datetime.fromtimestamp(package['metadata']['added']/1000, tz=datetime.timezone.utc))
         entry.updated(datetime.datetime.fromtimestamp(package['metadata']['lastUpdated']/1000, tz=datetime.timezone.utc))
         entry.author(name=package['metadata'].get('authorName', ''))
+    
         title = package['metadata']['name'].get(lang, None)
         if title is None:
-            # fallback to firsr key
-            title = package['metadata']['name'][list(package['metadata']['name'].keys())[0]]
+            title_langs = list(package['metadata']['name'].keys())
+            # fallback to same language, but different area
+            if area_lang is not None:
+                for title_lang in title_langs:
+                    if title_lang.split('-')[0] == primary_lang:
+                        title = package['metadata']['name'][title_lang]
+                        break
+            # fallback to English, en-US first
+            if title is None:
+                if 'en-US' in title_langs:
+                    title = package['metadata']['name']['en-US']
+                for title_lang in title_langs:
+                    if title_lang.split('-')[0] == 'en':
+                        title = package['metadata']['name'][title_lang]
+                        break
+            # fallback to first key
+            if title is None:
+                title = package['metadata']['name'][title_langs[0]]
+        assert title is not None
         entry.title(title)
+
         entry.link(href=f'https://f-droid.org/packages/{package_name}/')
-        # RSS description == F-Droid summary
-        description = package['metadata'].get('summary', None)
+
+        # ATOM summary == F-Droid summary
+        description = package['metadata'].get('summary', None) # some app has no summary
         if description is not None:
             description = description.get(lang, None)
             if description is None:
+                desc_langs = list(package['metadata']['summary'].keys())
+                # fallback to same language, but different area
+                if area_lang is not None:
+                    for desc_lang in desc_langs:
+                        if desc_lang.split('-')[0] == primary_lang:
+                            description = package['metadata']['summary'][desc_lang]
+                            break
+                # fallback to English, en-US first
+                if description is None:
+                    if 'en-US' in desc_langs:
+                        description = package['metadata']['summary']['en-US']
+                    for desc_lang in desc_langs:
+                        if desc_lang.split('-')[0] == 'en':
+                            description = package['metadata']['summary'][desc_lang]
+                            break
                 # fallback to firsr key
-                description = package['metadata']['summary'][list(package['metadata']['summary'].keys())[0]]
-        entry.description(description, isSummary=True)
-        # RSS content == F-Droid description
-        content = package['metadata'].get('description')
+                if description is None:
+                    description = package['metadata']['summary'][list(package['metadata']['summary'].keys())[0]]
+        entry.summary(description if description else 'No summary.') # ATOM
+        entry.description(description if description else 'No summary.', isSummary=False) # RSS
+
+        # ATOM content == F-Droid description
+        content = package['metadata'].get('description', None)
         if content is not None:
             content = content.get(lang, None)
             if content is None:
+                content_langs = list(package['metadata']['description'].keys())
+                # fallback to same language, but different area
+                if area_lang is not None:
+                    for content_lang in content_langs:
+                        if content_lang.split('-')[0] == primary_lang:
+                            content = package['metadata']['description'][content_lang]
+                            break
+                # fallback to English, en-US first
+                if content is None:
+                    if 'en-US' in content_langs:
+                        content = package['metadata']['description']['en-US']
+                    for content_lang in content_langs:
+                        if content_lang.split('-')[0] == 'en':
+                            content = package['metadata']['description'][content_lang]
+                            break
                 # fallback to firsr key
                 content = package['metadata']['description'][list(package['metadata']['description'].keys())[0]]
-        entry.content(content)
-    os.makedirs('rss', exist_ok=True)
-    feed.rss_file(f'rss/new_apps.{lang}.xml', pretty=True)
+        if isinstance(content, str):
+            content = content.replace('\n', '<br>')
+        content = content if content else 'No description.'
+        entry.content(content, type='CDATA')
+    feed.atom_file(f'feed/new_apps.{lang}.xml', pretty=True)
+    feed.rss_file(f'feed/new_apps.{lang}.rss.xml', pretty=True)
+
+with open('index_template.html', 'r') as f:
+    index_template = f.read()
+html_body = ''
+for lang in langs:
+    html_body += f'<a href="new_apps.{lang}.xml">{lang} ATOM</a> (suggest) <br>\n'
+    html_body += f'<a href="new_apps.{lang}.rss.xml">{lang} RSS</a><br>\n'
+with open('feed/index.html', 'w') as f:
+    f.write(index_template.replace('{{body}}', html_body))
+
+print('Done.')
